0813a4e0eece0527b8bf9a4ba432e2b9323ce716
Kritchai Phromros <ky@pixka.me>
2023-12-18 11:21:40 +0700

    fixbug

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,10 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include "checkconnection.h"
+#include "html.h"
 
 #define jsonbuffersize 1024
-const String version = "116";
+const String version = "118";
 String name = "d1io";
 const String type = "D1IO";
 void loadconfigtoram();
@@ -153,11 +154,14 @@ void loadconfigtoram()
   configdata.checkintime = cfg.getIntConfig("checkintime", 600);
   configdata.otatime = cfg.getIntConfig("otatime", 600);
   configdata.ntpupdatetime = cfg.getIntConfig("ntpupdatetime", 600);
-  configdata.wifitimeout = cfg.getIntConfig("wifitimeout", 60);
+  configdata.wifitimeout = cfg.getIntConfig("checkconnectiontime", 600);
   configdata.readdhttime = cfg.getIntConfig("readdhttime", 60);
   otahost = cfg.getConfig("otahost", "point.pixka.me");
 }
-
+unsigned long getUptime()
+{
+  return millis()/1000;
+}
 void configdatatofile()
 {
   // cfg.addConfig("va0", configdata.va0);
@@ -201,17 +205,6 @@ void initConfig()
   cfg.addConfig("haveoled", 0);
 }
 Portio ports[ioport];
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>ESP WIFI </title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head><body>
-  <form action="/get">
-    SSID: <input type="text" name="ssid">
-    PASSWORD: <input type="password" name="password">
-    <input type="submit" value="Submit">
-  </form><br>
-</body></html>)rawliteral";
 
 void setAPMode()
 {
@@ -321,7 +314,7 @@ String makestatus()
   dd["heap"] = system_get_free_heap_size();
   dd["h"] = pfHum;
   dd["t"] = pfTemp;
-  dd["uptime"] = uptime;
+  dd["uptime"] = getUptime();
   dd["message"] = message;
   dd["dhtbuffer.time"] = dhtbuffer.count;
   dd["porttrick"] = porttrick;
@@ -354,6 +347,7 @@ String makestatus()
   dd["D8closetime"] = ports[5].closetime;
   dd["D8delay"] = ports[5].delay;
   dd["config.havetorestart"] = configdata.havetorestart;
+  dd["havetorestart"] = configdata.havetorestart;
   dd["config.restarttime"] = configdata.restarttime;
   dd["config.havedht"] = configdata.havedht;
   dd["restarttime"] = restarttime;
@@ -366,6 +360,8 @@ String makestatus()
   dd["load"] = load;
   dd["loadav"] = loadav;
   dd["status"] = runstatus;
+  dd["wifitimeout"] = wifitimeout;
+  dd["config.connectiontime"] = configdata.wifitimeout;
   serializeJsonPretty(dd, b, f);
   return String(b);
 }
@@ -559,6 +555,7 @@ void flip()
 // สำหรับนับว่า port ไหนทำงานบ้าง
 void portcheck()
 {
+  int d = 0;
   for (int i = 0; i < ioport; i++)
   {
     //  Serial.println("Check port " + ports[i].port);
@@ -569,6 +566,7 @@ void portcheck()
       Serial.print(ports[i].port);
       Serial.print(" Delay ");
       Serial.println(ports[i].delay);
+
       if (ports[i].delay == 0)
       {
         ports[i].run = 0;
@@ -576,9 +574,11 @@ void portcheck()
         Serial.println("End job");
       }
       else
-        runstatus = 1;
+        d = 1; // มีคนที่ใช้ port อยู่
     }
   }
+
+  runstatus = d;
 }
 /**
  * @brief setup port mode
@@ -612,235 +612,7 @@ String intToEnable(int i)
     return String("Enable");
   }
 }
-const char configfile_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>Config</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta charset="UTF-8">
-  <style>
-    html {font-family: Arial; display: inline-block; text-align: center;}
-    h2 {font-size: 3.0rem;}
-    p {font-size: 3.0rem;}
-    body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
-    .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
-    .switch input {display: none}
-    .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 6px}
-    .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 3px}
-    input:checked+.slider {background-color: #b30000}
-    input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
 
-#customers {
-    /* font-family: 'Karla', Tahoma, Varela, Arial, Helvetica, sans-serif; */
-    border-collapse: collapse;
-    width: 100%%;
-    /* font-size: 12px; */
-}
-#btn {
-  border: 1px solid #777;
-  background: #6e9e2d;
-  color: #fff;
-  font: bold 11px 'Trebuchet MS';
-  padding: 4px;
-  cursor: pointer;
-  -moz-border-radius: 4px;
-  -webkit-border-radius: 4px;
-}
-.button {
-  background-color: #4CAF50; /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-}
-#customers td,
-#customers th {
-    border: 1px solid #ddd;
-    padding: 8px;
-}
-
-
-/* #customers tr:nth-child(even){background-color: #f2f2f2;} */
-
-#customers tr:hover {
-    background-color: #ddd;
-}
-
-#customers th {
-    padding-top: 12px;
-    padding-bottom: 12px;
-    text-align: left;
-    background-color: #4CAF50;
-    color: white;
-}
-</style>
-
-<script>
-function deleteallconfig()
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/resetconfig", true); 
-    xhr.send();
-}
-function remove(config)
-{
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "/removeconfig?configname="+config, true); 
-
-     xhr.addEventListener("readystatechange", () => {
-     console.log(xhr.readystate);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-     console.log(xhr.responseText);
-     location.reload();
-     }
- });
-    xhr.send();
-}
-function add()
-{
-  var xhr = new XMLHttpRequest();
-  var input = document.getElementById('newconfigname');
-  var value = document.getElementById('newvalue');
-  xhr.open("GET", "/setconfig?configname="+input.value+"&value="+value.value, true); 
-  xhr.addEventListener("readystatechange", () => {
-     console.log(xhr.readystate);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-     console.log(xhr.responseText);
-     var o =  JSON.parse(xhr.responseText);
-     var t = document.getElementById('customers');
-     var row = t.insertRow();
-     row.innerHTML = "<td>"+o.setconfig+"</td><td>"+o.value+"</td><td><input value="+o.value+"></td>";
-     }
- });
-  xhr.send();
-}
-function setvalue(element,configname,value) {
-  console.log("Call",element);
-  var xhr = new XMLHttpRequest();
-  var input = document.getElementById(configname);
-
-  xhr.open("GET", "/setconfig?configname="+configname+"&value="+input.value, true); 
-  xhr.addEventListener("readystatechange", () => {
-     console.log(xhr.readystate);
-    if (xhr.readyState === 4 && xhr.status === 200) {
-     console.log(xhr.responseText);
-    var o =  JSON.parse(xhr.responseText);
-  var showvalue = document.getElementById(configname+'value');  
-  console.log('Showvalue',showvalue);
-  console.log('O',o);
-  showvalue.innerHTML = o.value
-    } else if (xhr.readyState === 4) {
-     console.log("could not fetch the data");
-     }
-        });
-  xhr.send();
-}
-
-
-setInterval(()=>{
-  
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/", true); 
-  xhr.addEventListener("readystatechange", () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-    console.log(xhr.responseText);
-    var o =  JSON.parse(xhr.responseText);
-    console.log('O',o);
-    
-    var uptime = document.getElementById("uptime"); 
-    uptime.innerHTML = o.uptime 
-
-    var d1 = document.getElementById("d1"); 
-    d1.innerHTML = o.d1 
-
-var d2 = document.getElementById("d2"); 
-    d2.innerHTML = o.d2 
-
-var d3 = document.getElementById("d3"); 
-    d3.innerHTML = o.d3
-var d4 = document.getElementById("d4"); 
-    d4.innerHTML = o.d4
-var d5 = document.getElementById("d5"); 
-    d5.innerHTML = o.d5 
-
-var d6 = document.getElementById("d6"); 
-    d6.innerHTML = o.d6
-var d7 = document.getElementById("d7"); 
-    d7.innerHTML = o.d7
-var d8 = document.getElementById("d8"); 
-    d8.innerHTML = o.d8
-var t = document.getElementById("t"); 
-    t.innerHTML = o.t
-    var h = document.getElementById("h"); 
-    h.innerHTML = o.h
-    var version = document.getElementById("version"); 
-    version.innerHTML = o.version
-    var heap = document.getElementById("heap"); 
-    heap.innerHTML = o.heap
-       var name = document.getElementById("name"); 
-    name.innerHTML = o.name
-
-    } else if (xhr.readyState === 4) {
-     console.log("could not fetch the data");
-     }
-    });
-  xhr.send();
-  console.log('Call refresh');
-}
-, 500);
-
-
-</script>
-  </head><body>
- <table id="customers">
-  <tr>
-  <td>Config</td><td>value</td><td>Set</td><td>#</td><td>x</td>
-  </tr>
-  %CONFIG%
- </table>
-<hr>
-New Config <input id=newconfigname> <input id=newvalue> <button  id=btn onClick="add()">add </button>
-<hr>
-<button id=btn onClick="deleteallconfig()">Reset Config</button>
-<table id="customers">
-<tr>
-  <td>version</td><td><label id="version">0</label></td>
-    </tr><tr> 
-    <tr>
-  <td>heap</td><td><label id="heap">0</label></td>
-    </tr><tr> 
- <tr>
-  <td>uptime</td><td><label id="uptime">0</label></td>
-    </tr><tr> 
-  <td>D1</td><td><label id="d1">0</label></td>
-    </tr><tr> 
-  <td>D2</td><td><label id="d2">0</label></td>
-    </tr><tr> 
-  <td>D3</td><td><label id="d3">0</label></td>
-    </tr><tr> 
-  <td>D4</td><td><label id="d4">0</label></td>
-   </tr><tr> 
-  <td>D5</td><td><label id="d5">0</label></td>
-    </tr><tr> 
-  <td>D6</td><td><label id="d6">0</label></td>
-    </tr><tr> 
-  <td>D7</td><td><label id="d7">0</label></td>
-   </tr><tr> 
-  <td>D8</td><td><label id="d8">0</label></td>
-    </tr>
-    <tr>
-  <td>a0</td><td><label id="a0">0</label></td>
-     </tr><tr>  
-  <td>t</td><td><label id="t">0</label></td>
-  </tr>
-       <tr>
-  <td>h</td><td><label id="h">0</label></td>
-  </tr>
-
- </table>
-</body></html>)rawliteral";
 String fillconfig(const String &var)
 {
   // Serial.println(var);
@@ -1064,6 +836,7 @@ void checkintask()
     checkin();
   }
 }
+
 void otatask()
 {
   if (otatime > configdata.otatime)
@@ -1095,18 +868,16 @@ void checkconneciontask()
   if (wifitimeout > configdata.wifitimeout && !runstatus)
   {
     wifitimeout = 0;
-    if (WiFi.status() != WL_CONNECTED)
+
+    int re = talktoServer(WiFi.localIP().toString(), name, uptime, &cfg);
+    Serial.printf("\n Check connection return : %d\n", re);
+    if (re != 200 && configdata.havetorestart)
+    {
+      ESP.restart();
+    }
+    else
     {
       WiFi.reconnect();
-    }
-    else if (!runstatus && wifitimeout > configdata.wifitimeout)
-    { // ใช้สำหรับ check ว่า ยังติดต่อ server ได้เปล่าถ้าได้ก็ผ่านไป
-
-      int re = talktoServer(WiFi.localIP().toString(), name, uptime, &cfg);
-      if (re != 200 && configdata.havetorestart)
-      {
-        ESP.restart();
-      }
     }
   }
 }
